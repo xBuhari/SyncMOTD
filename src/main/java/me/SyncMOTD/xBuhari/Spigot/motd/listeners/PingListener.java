ef05b7ace5a6a702bfb59ae1e461c6b697b144fc
M. Buhari <muhammetbuharigokoz@gmail.com>
2021-01-30 21:37:36 +0300

    Set Icon

--- src/main/java/me/SyncMOTD/xBuhari/Spigot/motd/listeners/PingListener.java
+++ src/main/java/me/SyncMOTD/xBuhari/Spigot/motd/listeners/PingListener.java
@@ -1,13 +1,22 @@
 package me.SyncMOTD.xBuhari.Spigot.motd.listeners;
 
+import me.SyncMOTD.xBuhari.Spigot.SyncMOTD;
+import me.SyncMOTD.xBuhari.Spigot.motd.MotdManager;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
 
 public class PingListener implements Listener {
 
-    @EventHandler
-    public void onServerPing(ServerListPingEvent e) {
+    private MotdManager motdManager;
 
+    public PingListener() {
+        this.motdManager = SyncMOTD.getPlugin().getMotdManager();
+    }
+
+    @EventHandler
+    public void onServerPing(ServerListPingEvent e) throws Exception {
+        e.setServerIcon(Bukkit.loadServerIcon(this.motdManager.getRemoteServer().getServerIcon()));
     }
 }
