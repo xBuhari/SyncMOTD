e0a6e88f9cffa3062c5c4f7d677de428cce1d5dd
M. Buhari <muhammetbuharigokoz@gmail.com>
2021-01-30 20:38:14 +0300

    Proxy System.

--- src/main/java/me/SyncMOTD/xBuhari/Motd/RemoteServer.java
+++ src/main/java/me/SyncMOTD/xBuhari/Motd/RemoteServer.java
@@ -15,24 +15,54 @@ import java.net.URL;
 import java.net.URLConnection;
 
 public class RemoteServer {
-    private String name, ip, status;
 
+    //JSON
     private String alltext;
 
     //Proxy
+    private Boolean useProxy;
+    private String ProxyIP;
+    private Integer ProxyPort;
+    private Type ProxyType;
 
+    //Server
+    private String name, ip, status;
     private int port;
     private int playercount;
     private int maxpcount;
 
     public RemoteServer(String ip, int port) {
+        this.useProxy = false;
         this.ip = ip;
         this.port = port;
     }
 
     public RemoteServer(String ip, int port, String proxyIP, Integer proxyPort, String proxyType) {
+        this.useProxy = true;
         this.ip = ip;
         this.port = port;
+        this.ProxyIP = proxyIP;
+        this.ProxyPort = proxyPort;
+        switch (proxyType) {
+            case "HTTP":
+                this.ProxyType = Type.HTTP;
+                break;
+            case "HTTPS":
+                this.ProxyType = Type.HTTP;
+                break;
+            case "SOCKS":
+                this.ProxyType = Type.SOCKS;
+                break;
+            case "SOCKS4":
+                this.ProxyType = Type.SOCKS;
+                break;
+            case "SOCKS5":
+                this.ProxyType = Type.SOCKS;
+                break;
+            default:
+                this.useProxy = false;
+                break;
+        }
     }
 
     public String getName() {
@@ -60,25 +90,31 @@ public class RemoteServer {
     }
 
     public void update() throws IOException, ParseException {
-        Proxy proxy = new Proxy(Type.SOCKS, new InetSocketAddress("188.166.34.137", 9001));
         URL url = new URL("https://api.mcsrvstat.us/2/mc.hypixel.net");
-        URLConnection con = url.openConnection(proxy);
-        InputStream in = con.getInputStream();
-        String encoding = con.getContentEncoding();
+
+        URLConnection urlConnection;
+        if (useProxy) {
+            Proxy proxy = new Proxy(this.ProxyType, new InetSocketAddress(this.ProxyIP, this.ProxyPort));
+            urlConnection = url.openConnection(proxy);
+        }
+        else {
+            urlConnection = url.openConnection();
+        }
+
+        InputStream in = urlConnection.getInputStream();
+
+        String encoding = urlConnection.getContentEncoding();
         encoding = encoding == null ? "UTF-8" : encoding;
+
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
         byte[] buf = new byte[8192];
         int len = 0;
         while ((len = in.read(buf)) != -1) {
             baos.write(buf, 0, len);
         }
-        String body = new String(baos.toByteArray(), encoding);
-        JSONParser parser = new JSONParser();
-        JSONObject json = (JSONObject) parser.parse(body);
+        this.alltext = new String(baos.toByteArray(), encoding);
 
-        System.out.println(json.get("ip"));
-
-        alltext = body;
     }
 
     public void updateVariables() {
